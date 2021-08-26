bde13ce00961d63f9d9cb5c8c19324731ef71030
sam bacha <sam@manifoldfinance.com>
2021-08-26 04:57:04 -0700

    refactor(solidity): fix imports, update interfaces and additional api upgrades
    Signed-off-by: sam bacha <sam@manifoldfinance.com>

--- contracts/aave/ILendingPoolAddressesProvider.sol
+++ contracts/aave/ILendingPoolAddressesProvider.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.6;
+pragma solidity >=0.6.0 <0.8.0;
 
 /**
     @title ILendingPoolAddressesProvider interface
@@ -6,6 +6,47 @@ pragma solidity ^0.6.6;
  */
 
 interface ILendingPoolAddressesProvider {
-    function getLendingPoolCore() external view returns (address payable);
-    function getLendingPool() external view returns (address);
+  event LendingPoolUpdated(address indexed newAddress);
+  event ConfigurationAdminUpdated(address indexed newAddress);
+  event EmergencyAdminUpdated(address indexed newAddress);
+  event LendingPoolConfiguratorUpdated(address indexed newAddress);
+  event LendingPoolCollateralManagerUpdated(address indexed newAddress);
+  event PriceOracleUpdated(address indexed newAddress);
+  event LendingRateOracleUpdated(address indexed newAddress);
+  event ProxyCreated(bytes32 id, address indexed newAddress);
+  event AddressSet(bytes32 id, address indexed newAddress, bool hasProxy);
+
+  function setAddress(bytes32 id, address newAddress) external;
+
+  function setAddressAsProxy(bytes32 id, address impl) external;
+
+  function getAddress(bytes32 id) external view returns (address);
+
+  function getLendingPool() external view returns (address);
+
+  function setLendingPoolImpl(address pool) external;
+
+  function getLendingPoolConfigurator() external view returns (address);
+
+  function setLendingPoolConfiguratorImpl(address configurator) external;
+
+  function getLendingPoolCollateralManager() external view returns (address);
+
+  function setLendingPoolCollateralManager(address manager) external;
+
+  function getPoolAdmin() external view returns (address);
+
+  function setPoolAdmin(address admin) external;
+
+  function getEmergencyAdmin() external view returns (address);
+
+  function setEmergencyAdmin(address admin) external;
+
+  function getPriceOracle() external view returns (address);
+
+  function setPriceOracle(address priceOracle) external;
+
+  function getLendingRateOracle() external view returns (address);
+
+  function setLendingRateOracle(address lendingRateOracle) external;
 }
